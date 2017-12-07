8ebf916aedeae63ba1e32a1b98e29c9cf0104022
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-07 14:22:32 -0500

    Updated from Brython Server: 12/7/2017 2:22:32 PM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,15 @@
+#Andrew Parker
 
+
+dictionary = open('engmix.txt')
+
+i = 0
+x = 'b'
+for word in dictionary:
+    length = len(word)
+    if length > i:
+        i = length
+        x = word
+
+print(i)
+print(x)
