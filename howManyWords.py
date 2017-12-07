7c80adfb28e938bb3af0d439a55f88862fa97f51
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-07 14:49:03 -0500

    Updated from Brython Server: 12/7/2017 2:49:02 PM

--- howManyWords.py
+++ howManyWords.py
@@ -4,13 +4,18 @@ dictionary = open('engmix.txt')
 
 n = [0]*22
 
-i = 1
-x = 1
+
 for word in dictionary:
-    length = len(word)
+    length = len(word.strip())
     
     n[length] = n[length] + 1
     
 print(n)
 
-
+i = 0
+x = 1
+for num in n:
+    if i<23:
+        print('There are', n[i], 'words with', x,'letters')
+i = i+1
+x = x+1
