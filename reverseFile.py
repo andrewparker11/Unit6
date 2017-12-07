22eaa8ce5ebc762cbfe1ad2ca6987f64c1287ff1
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-07 14:56:05 -0500

    Updated from Brython Server: 12/7/2017 2:56:04 PM

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,22 @@
+#Andrew Parker
+#Dec 7 2017
 
+dictionary = open('engmix.txt')
+
+n = [0]*22
+
+
+for word in dictionary:
+    length = len(word.strip())
+    
+    n[length] = n[length] + 1
+    
+print(n)
+
+i = 0
+x = 1
+for num in n:
+    if i<23:
+        print('There are', n[i], 'words with', x,'letters')
+    i = i+1
+    x = x+1
