ca86e33a4f792b7943de13b84914021b34069359
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-08 14:45:45 -0500

    Updated from Brython Server: 12/8/2017 2:45:45 PM

--- dictionarySearcher.py
+++ dictionarySearcher.py
@@ -1,1 +1,14 @@
+#Andrew Parker
 
+dictionary = open('engmix.txt')
+
+stuff = input('Enter a word: ')
+
+word = []
+
+for line in dictionary:
+    word.append(line.strip())
+    
+for w in word:
+    if stuff in word:
+        print('True')
