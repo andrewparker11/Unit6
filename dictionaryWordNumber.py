48280ea2f657ba96f0456f0c0ab3ae82cf0f9213
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-08 14:52:23 -0500

    Updated from Brython Server: 12/8/2017 2:52:22 PM

--- dictionaryWordNumber.py
+++ dictionaryWordNumber.py
@@ -1,1 +1,14 @@
+#Andrew Parker
+#Dec. 8, 2017
+#Dictionary Word Number
 
+dictionary = open('engmix.txt')
+
+num = int(input('Enter a number: ')
+
+word = []
+
+for line in dictionary:
+    word.append(line.strip())
+    
+print(word[num])
