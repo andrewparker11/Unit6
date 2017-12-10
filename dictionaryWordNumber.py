a8e3898f84d4330d07ce8774e1e05c433aec1199
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-10 00:50:28 -0500

    Updated from Brython Server: 12/10/2017 12:50:27 AM

--- dictionaryWordNumber.py
+++ dictionaryWordNumber.py
@@ -1,5 +1,23 @@
 #Andrew Parker
 #Dec. 8, 2017
+
+#Dictionary Searcher
+dictionary = open('engmix.txt')
+
+word = input('Enter a word: ')
+
+words = []
+
+for line in dictionary:
+    words.append(line.strip())
+    
+if word in words:
+    print(word, 'is in the dictionary')
+else: 
+    print(word, 'is not in the dictionary')
+
+
+
 #Dictionary Word Number
 
 dictionary = open('engmix.txt')
@@ -12,3 +30,15 @@ for line in dictionary:
     word.append(line.strip())
     
 print(word[num-1])
+
+
+
+#File Exclamation Adder
+file = open('input.txt')
+
+file2 = []
+
+for line in file:
+    file2.append(line.strip())
+    file2 + '!'
+print(file2)
