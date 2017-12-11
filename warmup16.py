50afd2f49b32a574a74ff710f5f0a57bb3a8d64b
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-11 14:36:52 -0500

    Updated from Brython Server: 12/11/2017 2:36:51 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,18 @@
+#Andrew Parker
+#Dec. 11, 2017
+#warmup16.py - find all words in the dictionary that start with your first initial and end with your last initial
 
+#Dictionary Searcher
+dictionary = open('engmix.txt')
+
+
+words = []
+
+for line in dictionary:
+    words.append(line.strip())
+    
+
+for word in words:
+    if word[0] = a and word[-1] = p:
+        print word
+        
