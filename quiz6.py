d27921386e846f3f2bf2aa7208029b2316f34b0e
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-13 13:51:20 -0500

    Updated from Brython Server: 12/13/2017 1:51:20 PM

--- quiz6.py
+++ quiz6.py
@@ -1,27 +1,39 @@
 #Andrew Parker
 #Dec. 13, 2017
 #quiz6
-"""
+
 #Program 1 
 dictionary = open('engmix.txt')
-
-word = input('Enter a word: ')
 
 words = []
 
 for line in dictionary:
     words.append(line.strip())
-    
-if word in words:
-    print(word, 'is in the dictionary')
-else: 
-    print(word, 'is not in the dictionary')
 
-
+for word in words:
+    if word.count('c') == 3:
+        if word.count('p') == 2:
+            print(word)
 
 
 
 """
+
+#Program 2 
+dictionary = open('engmix.txt')
+
+words = []
+
+for line in dictionary:
+    words.append(line.strip())
+
+for word in words:
+    if len(word) > 0:
+        if word[0] == 'r':
+            print(word)
+        
+
+
 
 #Program 3
 dictionary = open('engmix.txt')
@@ -41,27 +53,6 @@ for word in words:
 
 
 
-
-
-
-"""
-#Program 2 
-dictionary = open('engmix.txt')
-
-words = []
-
-for line in dictionary:
-    words.append(line.strip())
-
-for word in words:
-    if len(word) > 0:
-        if word[0] == 'r':
-            print(word)
-        
-"""   
-
-
-"""
 #Program 5 
 
 dictionary = open('engmix.txt')
