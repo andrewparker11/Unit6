f828c03c7bea8b5823c52673e65e06c94cf66b91
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-13 13:56:28 -0500

    Updated from Brython Server: 12/13/2017 1:56:28 PM

--- quiz6.py
+++ quiz6.py
@@ -2,6 +2,49 @@
 #Dec. 13, 2017
 #quiz6
 
+"""
+
+#Program 4
+dictionary = open('engmix.txt')
+letter = int(input('Enter a letter: ')
+
+words = []
+
+for line in dictionary:
+    words.append(line.strip())
+    
+for word in words:
+    if word :
+        if word.count('p') == 2:
+            print(word)
+
+
+
+dictionary = open('engmix.txt')
+
+n = [0]*22
+
+
+for word in dictionary:
+    length = len(word.strip())
+    
+    n[length] = n[length] + 1
+    
+print(n)
+
+i = 0
+x = 1
+for num in n:
+    if i<23:
+        print('There are', n[i], 'words with', x,'letters')
+    i = i+1
+    x = x+1
+
+"""
+
+
+
+
 #Program 1 
 dictionary = open('engmix.txt')
 
@@ -16,9 +59,7 @@ for word in words:
             print(word)
 
 
-
 """
-
 #Program 2 
 dictionary = open('engmix.txt')
 
@@ -33,6 +74,7 @@ for word in words:
             print(word)
         
 
+"""
 
 
 #Program 3
@@ -63,4 +105,3 @@ for line in dictionary:
     words.append(line.strip())
     
 print(words[len(words)/2])
-"""
